794699803b605cd78854e77e604cc7b664f7ab74
PothuMahesh <maheshpothu7@gmail.com>
2025-01-30 11:39:54 +0530

    day17

---
+++ day17/demo.js
@@ -0,0 +1,12 @@
+//default export
+// export let msg="hello good morning";
+// export function greet(){
+//     return msg+" i am coding now"
+// }
+
+//named export
+let msg="hello good morning";
+function greet(){
+    return msg+" i am coding now"
+}
+export {msg,greet}
---
+++ day17/demo2.js
@@ -0,0 +1,13 @@
+import {msg} from "./demo.js"
+import {greet} from "./demo.js"
+let abc=greet();
+console.log(abc)
+console.log(greet())
+
+//creste a signup form that contains 
+//username(!empty),
+//email(@,!empty),
+//password (should be 6) and 
+//confirm password(same, !empty),
+//once you click on submit button , aalert should be popped up on your screen "form is submitted successfully"
+//if and only if all the validations are correct
